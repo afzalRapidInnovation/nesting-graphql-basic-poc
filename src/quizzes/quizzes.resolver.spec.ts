91eaf0e7bb727983633b64997faca7e49dcd729e
afzalRapidInnovation <111049931+afzalRapidInnovation@users.noreply.github.com>
2022-10-14 17:44:22 +0530

    Update quizzes.resolver.spec.ts

--- src/quizzes/quizzes.resolver.spec.ts
+++ src/quizzes/quizzes.resolver.spec.ts
@@ -16,3 +16,31 @@ describe('QuizzesResolver', () => {
     expect(resolver).toBeDefined();
   });
 });
+
+
+@Resolver((of) => Employee)
+export class EmployeeResolver{
+   @Query((returns) => Employee, {
+        name: EmployeeServiceConstant.getEmployeeById
+    })
+    async getEmployeeById(
+        @Args('input') getEmployeeByIdInput: GetEmployeeByIdInput
+    ) {
+        let data = await this.employeeService
+            .getEmployeeById(getEmployeeByIdInput)
+            .toPromise();
+        return data;
+    }
+
+    @ResolveField((returns) => EmployeeDocuments)
+    async employeeDocuments(
+        @Parent() getEmployeeById:EmployeeDef
+    ) {
+        const data = await this.employeeService
+            .getEmployeeDocumentsById({employeeId})
+            .toPromise();
+            console.log(data)
+        return data;
+    }
+
+}
